a3893cd51180adf6f3b2b17048bf75e7bb4e7c1d
KARPED1EM <89320463+KARPED1EM@users.noreply.github.com>
2022-12-09 15:35:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const Application = require('@waline/vercel');
 
+Waline.init({
+  el: '#waline',
+  search: false,
+  emoji: [
+      '//npm.elemecdn.com/waline-magic@latest/TV',
+      '//npm.elemecdn.com/waline-magic@latest/Heybox',
+      '//npm.elemecdn.com/waline-magic@latest/2233',
+      '//npm.elemecdn.com/waline-magic@latest/Menhera-chan',
+    ],
+});
+
+
 module.exports = Application({
   async postSave(comment) {
     // do what ever you want after save comment
   },
+  forbiddenWords: ['文开', '开文'],
 });
